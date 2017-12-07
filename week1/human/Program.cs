46cf0cbec42493cd83308b98611a80ad3f35e34f
mikegerrity <mikegerrity@hotmail.com>
2017-12-07 09:13:48 -0500

    added human

---
+++ week1/human/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace human
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Human player1 = new Human("Luke Skywalker",20,1,16,500);
+            Human player2 = new Human("Darth Vadar",20,75,88,600);
+            player1.Attack(player2);
+            Console.WriteLine($"{player1.name} attacks {player2.name}.  {player2.name}'s health is now {player2.health}");
+        }
+    }
+}
